3e528fbef2aa57b31417abb2a5b441e9d2b05654
Abdulla Karjikar <abdulla.karjikar@okstate.edu>
2022-02-19 17:03:54 -0600

    Initial Commit

---
+++ Ind02_Karjikar_Abdulla/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ind02_Karjikar_Abdulla
+//
+//  Created by Abdulla Karjikar on 2/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
