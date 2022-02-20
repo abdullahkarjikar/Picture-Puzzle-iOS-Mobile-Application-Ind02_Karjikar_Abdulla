bfad72a8c3472906dfc0ced8079cf7190721b94d
Abdulla Karjikar <abdulla.karjikar@okstate.edu>
2022-02-19 19:12:55 -0600

    Added Outlet for Image View and Added Tap gesture recognizer for Img_1_1

--- Ind02_Karjikar_Abdulla/ViewController.swift
+++ Ind02_Karjikar_Abdulla/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Img_1_1: UIImageView!
+    @IBOutlet weak var Img_1_2: UIImageView!
+    @IBOutlet weak var Img_1_3: UIImageView!
+    @IBOutlet weak var Img_1_4: UIImageView!
+    
+    @IBOutlet weak var Img_2_1: UIImageView!
+    @IBOutlet weak var Img_2_2: UIImageView!
+    @IBOutlet weak var Img_2_3: UIImageView!
+    @IBOutlet weak var Img_2_4: UIImageView!
+    
+    @IBOutlet weak var Img_3_1: UIImageView!
+    @IBOutlet weak var Img_3_2: UIImageView!
+    @IBOutlet weak var Img_3_3: UIImageView!
+    @IBOutlet weak var Img_3_4: UIImageView!
+    
+    @IBOutlet weak var Img_4_1: UIImageView!
+    @IBOutlet weak var Img_4_2: UIImageView!
+    @IBOutlet weak var Img_4_3: UIImageView!
+    @IBOutlet weak var Img_4_4: UIImageView!
+
+    @IBOutlet weak var Img_5_1: UIImageView!
+    @IBOutlet weak var Img_5_2: UIImageView!
+    @IBOutlet weak var Img_5_3: UIImageView!
+    @IBOutlet weak var Img_5_4: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func TapHandler(_ sender: UITapGestureRecognizer) {
+        
+        let temp : UIImageView = sender.view as! UIImageView
+        print(temp.frame)
+        
+        print(Img_1_1.frame)
+        
+    }
+    
 
 }
 
